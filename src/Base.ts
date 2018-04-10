c2992108c8db6e52e574b55859bfe6fc34e02cc2
llafuente <llafuente@noboxout.com>
2018-04-10 11:54:46 +0200

    feat: add bindParameter and onParameterChange

--- src/Base.ts
+++ src/Base.ts
@@ -1,13 +1,23 @@
 import { OnInit, OnDestroy, Injector } from "@angular/core";
 import { Subscription } from "rxjs/Rx" ;
 import { Observable } from "rxjs/Observable" ;
-import { ActivatedRoute, ActivatedRouteSnapshot, Router } from "@angular/router";
+import { ActivatedRoute, ActivatedRouteSnapshot, Router, Params } from "@angular/router";
 import { BBAlertService, BBConfirmService, ToastyService, ToastOptions, ToastData } from "angular-bootstrap-ui";
 
 import { LogService } from "./services/Log.service";
 import { Logger } from "./services/Logger";
 import { SessionStorageService } from "./services/SessionStorage.service";
 
+/**
+ * Base class for componets to extends that handle many common staff
+ * * route, parameters and resolved
+ * * router
+ * * modal confirmations
+ * * modal alerts
+ * * unsubscription to subscriptions when component is destroyed
+ * * log
+ * * session (localStorage)
+ */
 export class Base implements /*OnInit, */OnDestroy {
   subscriptions: Subscription[] = [];
   timeouts: number[] = [];
@@ -18,6 +28,10 @@ export class Base implements /*OnInit, */OnDestroy {
   confirmService: BBConfirmService;
   alertService: BBAlertService;
 
+  parameterListeners: {[paramName: string]: (value: any) => void} = {};
+  /**
+   * Fetch some things that most componets uses and init route listeners
+   */
   constructor(
     public injector: Injector,
     public route: ActivatedRoute
@@ -30,12 +44,51 @@ export class Base implements /*OnInit, */OnDestroy {
 
     const logs: LogService = injector.get(LogService);
     this.currentLog = logs.to("Component"); // maybe instaceof this
+
+    let r = route;
+    do {
+      this.handleSubscription(r.params.subscribe(this.parameterChanged.bind(this)));
+
+      r = r.parent;
+    } while(r && r.parent);
+  }
+  /**
+   * parameter changed listener
+   */
+  protected parameterChanged(p: Params) {
+    for (let idx in p) {
+      if (this.parameterListeners[idx]) {
+        this.parameterListeners[idx](p[idx]);
+      }
+    }
+  }
+  /**
+   * Notify given callback when a change in parameters is detected
+   * that not means that paramName is changed
+   */
+  onParameterChange(paramName: string, cb: (value: any) => void) {
+    this.parameterListeners[paramName] = cb;
   }
 
+  /**
+   * Keep your member in sync with a parameter with the same name
+   */
+  bindParameter(paramName) {
+    this[paramName] = this.getParameter(paramName);
+    this.onParameterChange(paramName, (p) => {
+      this[paramName] = p;
+    });
+  }
+  /**
+   * Add the subscription to a list that will unsubscribe when componet
+   * is destroyed
+   */
   handleSubscription(s: Subscription) {
     this.subscriptions.push(s);
   }
-
+  /**
+   * Create a timeout that is cleared if the componet is destroyed
+   */
   timeout(fn: Function, miliseconds: number): number {
     const t = setTimeout(fn, miliseconds);
     this.timeouts.push(t);
@@ -43,7 +96,9 @@ export class Base implements /*OnInit, */OnDestroy {
   }
 
   /**
-   * If You need to override ngOnDestroy remember to always call super()
+   * unsubscibe and clear timeouts
+   *
+   * NOTE: If You need to override ngOnDestroy remember to always call super()
    */
   ngOnDestroy() {
     this.subscriptions.forEach((subs) => {
@@ -54,7 +109,9 @@ export class Base implements /*OnInit, */OnDestroy {
       clearTimeout(this.timeouts[i]);
     }
   }
-
+  /**
+   * Create an error growl, require ng2-toasty to be configured
+   */
   errGrowl(str: string, timeout: number = 10000) {
     const toastOptions:ToastOptions = {
       title: str,
@@ -72,7 +129,9 @@ export class Base implements /*OnInit, */OnDestroy {
     // Add see all possible types in one shot
     this.toastyService.error(toastOptions);
   }
-
+  /**
+   * Create a warning growl, require ng2-toasty to be configured
+   */
   warnGrowl(str: string, timeout = 10000) {
     const toastOptions: ToastOptions = {
       title: str,
@@ -90,7 +149,9 @@ export class Base implements /*OnInit, */OnDestroy {
     // Add see all possible types in one shot
     this.toastyService.warning(toastOptions);
   }
-
+  /**
+   * Create an info growl, require ng2-toasty to be configured
+   */
   growl(str: string, timeout: number = 5000) {
     const toastOptions:ToastOptions = {
       title: str,
@@ -130,7 +191,10 @@ export class Base implements /*OnInit, */OnDestroy {
     this.currentLog.debug("getParameter", key, null);
     return null;
   }
-
+  /**
+   * Reverse ActivatedRoute and return the first non-undefined key in params
+   * casted to number (Integer)
+   */
   getNumberParameter(key: string): number|null {
     let snapshot: ActivatedRouteSnapshot = this.route.snapshot;
 
@@ -150,7 +214,7 @@ export class Base implements /*OnInit, */OnDestroy {
   }
 
   /**
-   * Reverse ActivatedRoute and return the first non-undefined key in params
+   * Reverse ActivatedRoute and return the first non-undefined key in data
    */
   getResolve<T>(key: string): T {
     let snapshot: ActivatedRouteSnapshot = this.route.snapshot;
@@ -169,11 +233,17 @@ export class Base implements /*OnInit, */OnDestroy {
     this.currentLog.debug("getResolve", key, null);
     return null;
   }
-
+  /**
+   * Create a confirmation modal
+   * bb-confirm-service need to be configured
+   */
   confirm(header: string, content: string): Observable<boolean> {
     return this.confirmService.show(header, content);
   }
-
+  /**
+   * Create a alert modal
+   * bb-alert-service need to be configured
+   */
   alert(header: string, content: string, isHTML: boolean): Observable<boolean> {
     return this.alertService.show(header, content, isHTML);
   }
